3749667f44684fbaab21e3601992bc00769021e5
Riccardo Gallo <riccardo.gallo@secomind.com>
2025-03-28 15:30:49 +0100

    fix(cmake): fix local cpp build process
    Signed-off-by: Riccardo Gallo <riccardo.gallo@secomind.com>

--- scripts/cpp_codegen.sh
+++ scripts/cpp_codegen.sh
@@ -5,39 +5,39 @@
 
 set -eEuo pipefail
 
-codegen () {
-    PROTO_DIR=$1
-    PROJECT_DIR=$2
-    OUT_DIR=$3
+codegen() {
+  PROTO_DIR=$1
+  PROJECT_DIR=$2
+  OUT_DIR=$3
 
-    # Remove old code
-    if [ -d "$OUT_DIR/astarteplatform" ]; then
-        rm -r "$OUT_DIR/astarteplatform"
-    fi
+  # Remove old code
+  if [ -d "$OUT_DIR/astarteplatform" ]; then
+    rm -r "$OUT_DIR/astarteplatform"
+  fi
 
-    mkdir -p $PROJECT_DIR/cmake/build
-    pushd $PROJECT_DIR/cmake/build
-    cmake -DOUT_FOLDER:STRING=$OUT_DIR -DPROTO_FOLDER:STRING=$PROTO_DIR ../..
-    make
-    popd
+  mkdir -p $PROJECT_DIR/cmake/build
+  pushd $PROJECT_DIR/cmake/build
+  cmake -GNinja -DOUT_FOLDER:STRING=$OUT_DIR -DPROTO_FOLDER:STRING=$PROTO_DIR ../..
+  cmake --build .
+  popd
 
-    }
+}
 
-install_code (){
-      OUT_DIR=$1
-      INSTALL_DIR=$2
+install_code() {
+  OUT_DIR=$1
+  INSTALL_DIR=$2
 
-      # Check if the directory exists and is not empty
-      if [ -d "$INSTALL_DIR"/astarteplatform/msghub ] && [ "$(ls -A "$INSTALL_DIR"/astarteplatform/msghub)" ]; then
-        rm -v "$INSTALL_DIR"/astarteplatform/msghub/*
-      fi
+  # Check if the directory exists and is not empty
+  if [ -d "$INSTALL_DIR"/astarteplatform/msghub ] && [ "$(ls -A "$INSTALL_DIR"/astarteplatform/msghub)" ]; then
+    rm -v "$INSTALL_DIR"/astarteplatform/msghub/*
+  fi
 
-      install -d "$INSTALL_DIR"/astarteplatform/msghub
-      install -m 644 "$OUT_DIR"/astarteplatform/msghub/* "$INSTALL_DIR"/astarteplatform/msghub
+  install -d "$INSTALL_DIR"/astarteplatform/msghub
+  install -m 644 "$OUT_DIR"/astarteplatform/msghub/* "$INSTALL_DIR"/astarteplatform/msghub
 }
 
 if [ "$1" = "codegen" ]; then
   codegen "$2" "$3" "$4"
 elif [ "$1" = "install_code" ]; then
-    install_code "$2" "$3"
+  install_code "$2" "$3"
 fi
